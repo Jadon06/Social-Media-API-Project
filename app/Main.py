9f33930db4f9db0abf1da0b710458308ce30fa94
jadon au-yeung <aycjadon@gmail.com>
2025-12-25 10:53:29 -0500

    Converted all SQL commands into queries using SQLAlchemy

--- app/Main.py
+++ app/Main.py
@@ -1,81 +1,110 @@
-from fastapi import FastAPI, Response, status, HTTPException
+# imports
+from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
+
 from pydantic import BaseModel
+
 import psycopg2
 from psycopg2.extras import RealDictCursor
+
+from . import models
+from .database import engine, get_db
+from sqlalchemy.orm import Session 
+
 from typing import Optional
 from random import randrange
 
+# creates all database tables defined within the models file
+models.Base.metadata.create_all(bind=engine)
+
+# app module for calling fastapi
 app = FastAPI()
 
+# connection module for linking postgres database with python program
 conn = psycopg2.connect(host='localhost', database='Social Media API Project Database', 
                         user='postgres', password='mommywoody5623', cursor_factory=RealDictCursor)
+# cursor module for executing sql methods
 cursor = conn.cursor()
 
+# Post model for validation
 class Post(BaseModel):
     title : str
     content : str
-    published : bool = False
+    published : Optional[bool] = False
     liked : Optional[bool] = None
 
-my_posts = [{"title" : "title of post 1", "content" : "content of post 1", "id" : 1}, 
-            {"title" : "title of post 2", "content" : "content of post 2", "id" : 2}]
+# NOTE - must pass in 'db: Session = Depends(get_db)' into the function argument to access table
 
-# find a post
-def find_post(id):
-    for post in my_posts:
-        if id == post["id"]:
-            return post
+# test query
+@app.get("/sqlalchemy")
+# Calling the Session function as an object and passing in the function 'get_db' as a argument of 'Depends' to make it a dependency
+def test_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    return {"Posts" : posts}
 
-# find post index
-def find_post_index(id):
-    for i in range(len(my_posts)):
-        if my_posts[i]["id"] == id:
-            return i
 
 # retrieve social media posts
 @app.get("/posts")
-def get_posts():
-    cursor.execute("""SELECT * FROM "Users";""")
-    posts = cursor.fetchall()
+def get_posts(db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * FROM "Posts";""")
+    # posts = cursor.fetchall() 
+    
+    posts = db.query(models.Post).all()
     return {"Data" : posts}
 
 # create social media posts
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(post: Post):
-    cursor.execute("""INSERT INTO "Users" ("title", "content", "published") VALUES (%s, %s, %s) RETURNING *;""",(
-                   post.title, post.content, post.published))
-    new_post = cursor.fetchall()
-    conn.commit()
+def create_post(post: Post, db: Session = Depends(get_db)):
+    # cursor.execute("""INSERT INTO "Posts" ("title", "content", "published") VALUES (%s, %s, %s) RETURNING *;""",(
+    #                post.title, post.content, post.published))
+    # new_post = cursor.fetchall()
+    # conn.commit()
+
+    new_post = models.Post(**post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
     return {"Post" : new_post}
 
 # retrieve a single post
 @app.get("/posts/{id}")
-def get_post(id : int):
-    cursor.execute("""SELECT * FROM "Users" WHERE "id" = %s;""",(str(id),))
-    post = cursor.fetchall()
+def get_post(id : int, db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * FROM "Posts" WHERE "id" = %s;""",(str(id),))
+    # post = cursor.fetchall()
+    
+    post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail="Post Not Found!!")
+
     return {"Post" : post}
 
 # update a post
 @app.put("/posts/{id}")
-def update_post(id : int, post : Post):
-    cursor.execute("""UPDATE "Users" SET "title" = %s, "content" = %s , published = %s WHERE "id" = %s RETURNING *;""", 
-                   (post.title, post.content, post.published, str(id),))
-    update_post = cursor.fetchone()
-    if not update_post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-    conn.commit()
+def update_post(id : int, post : Post, db: Session = Depends(get_db)):
+    # cursor.execute("""UPDATE "Posts" SET "title" = %s, "content" = %s , published = %s WHERE "id" = %s RETURNING *;""", 
+    #                (post.title, post.content, post.published, str(id),))
+    # update_post = cursor.fetchone()
+    # conn.commit()
+    updated_post = db.query(models.Post).filter(models.Post.id == id).first()
+    if not updated_post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post does not exist")
+    
+    updated_post = db.query(models.Post).filter(models.Post.id == id).update(post.dict(), synchronize_session=False)
+    db.commit()
     return {"data" : "post updated!!"} 
 
 # delete a post
 @app.delete("/posts/{id}")
-def delete_post(id : int):
-    cursor.execute("""DELETE FROM "Users" WHERE "id" = %s RETURNING *""", (str(id),))
-    post = cursor.fetchall()
+def delete_post(id : int, db: Session = Depends(get_db)):
+    # cursor.execute("""DELETE FROM "Posts" WHERE "id" = %s RETURNING *""", (str(id),))
+    # post = cursor.fetchall()
+    # conn.commit()
+
+    post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post Does Not Exist!")
-    conn.commit()
+    
+    db.query(models.Post).filter(models.Post.id == id).delete(synchronize_session=False)
+    db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
---
+++ app/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
+from .database import Base
+
+# Note - each class in the models file represents a table
+
+class Post(Base):
+    __tablename__ = "Posts"
+    
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    published = Column(Boolean, server_default='TRUE', nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    liked = Column(Boolean, nullable=True)
