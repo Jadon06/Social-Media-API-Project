21fd317234d39d8b93916a489b3f0e2a6627239d
jadon au-yeung <aycjadon@gmail.com>
2026-01-10 17:26:40 -0500

    added a function to make my virtual machine read the url

--- alembic/env.py
+++ alembic/env.py
@@ -8,7 +8,10 @@ from app.models import Base
 
 from app.config import settings
 
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
